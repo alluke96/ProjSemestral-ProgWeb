2b0f3cfe177f14fbc92e80e4636240482c32a13c
Allyson Dunke <alluke.pucpr@gmail.com>
2021-11-07 19:44:51 -0300

    Implements cadastro modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-function cadastro(){
-    alert("TODO")
-}
-
-function moreOptions(){
-    alert("TODO")
+function login(){
+  let login = document.getElementById('email_login')
+  let senha = document.getElementById('senha_login')
+  if(login.value == 'admin' && senha.value == 'admin'){
+    let res = document.getElementById('login')
+    res.innerText = 'admin'
+    alert('Bem vindo admin!')
+  }
 }
 
 function setResponsiveHamburger() {
@@ -16,7 +18,7 @@ function setResponsiveHamburger() {
   }
 
 // When the user scrolls the page, execute scrollLock
-window.onscroll = function() {scrollLock()};
+window.onscroll = function() {scrollLock(); scrollFunction();}
 
 function scrollLock() {
     var header = document.getElementById("myTopnav");
@@ -26,4 +28,19 @@ function scrollLock() {
     } else {
         header.classList.remove("sticky");
     }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn_scrollTop").style.display = "block";
+  } else {
+    document.getElementById("btn_scrollTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
