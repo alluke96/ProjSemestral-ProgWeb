88e5820f952888c99ffa65a05b4aacc7d3d06911
Allyson Dunke <alluke.pucpr@gmail.com>
2021-11-09 21:14:35 -0300

    Implements the basic semi-functional cart

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,26 +3,31 @@ var listaCarrinho = []
 window.onload = function () {
     listaCarrinho = JSON.parse(window.localStorage.getItem('carrinho'))
     console.log('Carrinho: ', listaCarrinho)
+    user = window.localStorage.getItem('user')
+    if(user == 'admin'){
+      let res = document.getElementById('login')
+      res.innerHTML = 'admin'
+      res.href = '' // removes the function to open modal
+    }
+
+    if(listaCarrinho != null){
+        montarCardProdutos()
+    }
 }
 
 function montarCardProdutos() {
-    document.getElementById('div-produto').innerHTML = ''
+    document.getElementById('div_carrinho').innerHTML = ''
   
-    for(var i = 0; i < listaProdutos.length; i++) {
+    for(var i = 0; i < listaCarrinho.length; i++) {
       var conteudo = ''
-      conteudo += `<div class="w3-card-4">`
-      conteudo += `<img class="card-img" src="` + listaProdutos[i][2] + `">`
-      conteudo += `<p>` + listaProdutos[i][1] + `</p>`
-      conteudo += `<p class="price"> R$ ` + listaProdutos[i][4] + `</p>`
-      if(listaProdutos[i][3] == false) {
-        conteudo += `<button class="w3-button w3-green" onclick="comprar(` + listaProdutos[i][0] + `)"> Comprar </button>`
-        conteudo += `</div>`
-      } else {
-        conteudo += `<button class="w3-button w3-disabled" onclick="comprar(` + listaProdutos[i][0] + `) disabled"> Adicionado ao carrinho </button>`
-        conteudo += `</div>`
-      }
+      conteudo += `<div class="w3-card-4 w3-margin-bottom w3-white crush">`
+      conteudo += `<img class="card_carrinho w3-margin-top" src="` + listaCarrinho[i][2] + `">`
+      conteudo += `<p>` + listaCarrinho[i][1] + `</p>`
+      conteudo += `<p class="price"> R$ ` + listaCarrinho[i][4] + `</p>`
+      conteudo += `<button class="w3-button w3-red w3-margin-bottom" onclick="comprar(` + listaCarrinho[i][0] + `)"> Remover </button>`
+      conteudo += `</div>`
       
   
-      document.getElementById('div-produto').innerHTML += conteudo
+      document.getElementById('div_carrinho').innerHTML += conteudo
     }
   }
